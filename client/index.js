21222c6cb3ebf1f75296a329641cb839d5f69d45
Sara Rose Gallagher <srgallag@gmail.com>
2017-11-22 11:07:43 -0500

    gotta add some api routes

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 const mapboxgl = require("mapbox-gl");
 const buildMarker = require("./marker.js");
 
-mapboxgl.accessToken = "YOUR API TOKEN HERE";
+mapboxgl.accessToken = "pk.eyJ1IjoiamFzbWluYWJhc3VyaXRhIiwiYSI6ImNqYTl0aHV0aDBraWEyd282cTduajhqb2YifQ.GG_9PlLoG9PCL2bcOQbYWg";
 
 const fullstackCoords = [-74.009, 40.705] // NY
 // const fullstackCoords = [-87.6320523, 41.8881084] // CHI
@@ -10,8 +10,11 @@ const map = new mapboxgl.Map({
   container: "map",
   center: fullstackCoords, // FullStack coordinates
   zoom: 12, // starting zoom
-  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+  style: "mapbox://styles/mapbox/dark-v9" // mapbox has lots of different map styles available.
 });
 
 const marker = buildMarker("activities", fullstackCoords);
 marker.addTo(map);
+
+
+fetch('/api/')
